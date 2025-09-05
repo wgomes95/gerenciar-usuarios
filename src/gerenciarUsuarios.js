53bbf405898ddc93b7229a77bc9963632dc178d1
Willian <willian.a.gomes95@gmail.com>
2025-09-04 22:58:09 -0300

    Adicionando funções de testes

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,14 @@
+const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+
+function retornarUsuarios() {
+    return usuarios;
+}
+
+function adicionarNovoUsuario(nome) {
+    usuarios.push(nome);
+}
+
+module.exports = {
+    retornarUsuarios,
+    adicionarNovoUsuario
+}
